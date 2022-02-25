snappautomotive/device_amlogic_yukawa

yukawa: Add HAL support for ECHO_REFERENCE audio source.
Bug: b/143616861
Test: Manual

Change-Id: I738d221930da6db9128ddd11415e3d4cdf85a06e

audio/audio_aec.h
@@ -36,12 +36,14 @@
 struct aec_t {
     pthread_mutex_t lock;
     size_t num_reference_channels;
+    bool mic_initialized;
     int32_t *mic_buf;
     size_t mic_num_channels;
     size_t mic_buf_size_bytes;
     size_t mic_frame_size_bytes;
     uint32_t mic_sampling_rate;
     struct aec_info last_mic_info;
+    bool spk_initialized;
     int32_t *spk_buf;
     size_t spk_num_channels;
     size_t spk_buf_size_bytes;
@@ -58,40 +60,33 @@ struct aec_t {
     bool prev_spk_running;
 };
 
-#ifdef AEC_HAL
-
-/* Write audio samples to AEC reference FIFO for use in AEC.
- * Both audio samples and timestamps are added in FIFO fashion.
- * Must be called after every write to PCM. */
-int write_to_reference_fifo (struct aec_t *aec, void *buffer, struct aec_info *info);
-
-/* Processing function call for AEC.
- * AEC output is updated at location pointed to by 'buffer'.
- * This function does not run AEC when there is no playback -
- * as communicated to this AEC interface using aec_set_spk_running().*/
-int process_aec (struct aec_t *aec, void* buffer, struct aec_info *info);
 
 /* Initialize AEC object.
  * This must be called when the audio device is opened.
- * ALSA device mutex must be held before calling this API.*/
+ * ALSA device mutex must be held before calling this API.
+ * Returns -EINVAL if AEC object fails to initialize, else returns 0. */
 int init_aec (int sampling_rate, int num_reference_channels,
                 int num_microphone_channels, struct aec_t **);
 
 /* Release AEC object.
  * This must be called when the audio device is closed. */
-void release_aec(struct aec_t *aec);
+void release_aec (struct aec_t *aec);
 
 /* Initialize reference configuration for AEC.
- * Must be called when a new output stream is opened. */
+ * Must be called when a new output stream is opened.
+ * Returns -EINVAL if any processing block fails to initialize,
+ * else returns 0. */
 int init_aec_reference_config (struct aec_t *aec, struct alsa_stream_out *out);
-
-/* Initialize microphone configuration for AEC.
- * Must be called when a new input stream is opened. */
-int init_aec_mic_config (struct aec_t *aec, struct alsa_stream_in *in);
 
 /* Clear reference configuration for AEC.
  * Must be called when the output stream is closed. */
 void destroy_aec_reference_config (struct aec_t *aec);
+
+/* Initialize microphone configuration for AEC.
+ * Must be called when a new input stream is opened.
+ * Returns -EINVAL if any processing block fails to initialize,
+ * else returns 0. */
+int init_aec_mic_config (struct aec_t *aec, struct alsa_stream_in *in);
 
 /* Clear microphone configuration for AEC.
  * Must be called when the input stream is closed. */
@@ -101,17 +96,34 @@ void destroy_aec_mic_config (struct aec_t *aec);
  * This is used by process_aec() to determine if AEC processing is to be run. */
 void aec_set_spk_running (struct aec_t *aec, bool state);
 
+/* Write audio samples to AEC reference FIFO for use in AEC.
+ * Both audio samples and timestamps are added in FIFO fashion.
+ * Must be called after every write to PCM.
+ * Returns -ENOMEM if the write fails, else returns 0. */
+int write_to_reference_fifo (struct aec_t *aec, void *buffer, struct aec_info *info);
+
+/* Get reference audio samples + timestamp, in the format expected by AEC,
+ * i.e. same sample rate and bit rate as microphone audio.
+ * Timestamp is updated in field 'timestamp_usec', and not in 'timestamp'.
+ * Returns:
+ *  -EINVAL    if the AEC object is invalid.
+ *  -ENOMEM    if the reference FIFO overflows or is corrupted.
+ *  -ETIMEDOUT if we timed out waiting for the requested number of bytes
+ *  0          otherwise */
+int get_reference_samples (struct aec_t *aec, void *buffer, struct aec_info *info);
+
+#ifdef AEC_HAL
+
+/* Processing function call for AEC.
+ * AEC output is updated at location pointed to by 'buffer'.
+ * This function does not run AEC when there is no playback -
+ * as communicated to this AEC interface using aec_set_spk_running().
+ * Returns -EINVAL if processing fails, else returns 0. */
+int process_aec (struct aec_t *aec, void* buffer, struct aec_info *info);
+
 #else /* #ifdef AEC_HAL */
 
-#define write_to_reference_fifo(...) ((int)0)
 #define process_aec(...) ((int)0)
-#define init_aec(...) ((int)0)
-#define release_aec(...) ((void)0)
-#define init_aec_reference_config(...) ((int)0)
-#define init_aec_mic_config(...) ((int)0)
-#define destroy_aec_reference_config(...) ((void)0)
-#define destroy_aec_mic_config(...) ((void)0)
-#define aec_set_spk_running(...) ((void)0)
 
 #endif /* #ifdef AEC_HAL */
 

ev-staging/android_hardware_amlogic_audio

Test: Manual, plus tests found at go/ag/9507428.
Change-Id: Iac114c0cdc706da88e88075dbe7225183b8e0a81

audio_aec.h
@@ -36,12 +36,14 @@
 struct aec_t {
     pthread_mutex_t lock;
     size_t num_reference_channels;
+    bool mic_initialized;
     int32_t *mic_buf;
     size_t mic_num_channels;
     size_t mic_buf_size_bytes;
     size_t mic_frame_size_bytes;
     uint32_t mic_sampling_rate;
     struct aec_info last_mic_info;
+    bool spk_initialized;
     int32_t *spk_buf;
     size_t spk_num_channels;
     size_t spk_buf_size_bytes;
@@ -58,40 +60,32 @@ struct aec_t {
     bool prev_spk_running;
 };
 
-#ifdef AEC_HAL
-
-/* Write audio samples to AEC reference FIFO for use in AEC.
- * Both audio samples and timestamps are added in FIFO fashion.
- * Must be called after every write to PCM. */
-int write_to_reference_fifo (struct aec_t *aec, void *buffer, struct aec_info *info);
-
-/* Processing function call for AEC.
- * AEC output is updated at location pointed to by 'buffer'.
- * This function does not run AEC when there is no playback -
- * as communicated to this AEC interface using aec_set_spk_running().*/
-int process_aec (struct aec_t *aec, void* buffer, struct aec_info *info);
-
 /* Initialize AEC object.
  * This must be called when the audio device is opened.
- * ALSA device mutex must be held before calling this API.*/
+ * ALSA device mutex must be held before calling this API.
+ * Returns -EINVAL if AEC object fails to initialize, else returns 0. */
 int init_aec (int sampling_rate, int num_reference_channels,
                 int num_microphone_channels, struct aec_t **);
 
 /* Release AEC object.
  * This must be called when the audio device is closed. */
-void release_aec(struct aec_t *aec);
+void release_aec(struct aec_t* aec);
 
 /* Initialize reference configuration for AEC.
- * Must be called when a new output stream is opened. */
+ * Must be called when a new output stream is opened.
+ * Returns -EINVAL if any processing block fails to initialize,
+ * else returns 0. */
 int init_aec_reference_config (struct aec_t *aec, struct alsa_stream_out *out);
-
-/* Initialize microphone configuration for AEC.
- * Must be called when a new input stream is opened. */
-int init_aec_mic_config (struct aec_t *aec, struct alsa_stream_in *in);
 
 /* Clear reference configuration for AEC.
  * Must be called when the output stream is closed. */
 void destroy_aec_reference_config (struct aec_t *aec);
+
+/* Initialize microphone configuration for AEC.
+ * Must be called when a new input stream is opened.
+ * Returns -EINVAL if any processing block fails to initialize,
+ * else returns 0. */
+int init_aec_mic_config(struct aec_t* aec, struct alsa_stream_in* in);
 
 /* Clear microphone configuration for AEC.
  * Must be called when the input stream is closed. */
@@ -101,17 +95,34 @@ void destroy_aec_mic_config (struct aec_t *aec);
  * This is used by process_aec() to determine if AEC processing is to be run. */
 void aec_set_spk_running (struct aec_t *aec, bool state);
 
+/* Write audio samples to AEC reference FIFO for use in AEC.
+ * Both audio samples and timestamps are added in FIFO fashion.
+ * Must be called after every write to PCM.
+ * Returns -ENOMEM if the write fails, else returns 0. */
+int write_to_reference_fifo(struct aec_t* aec, void* buffer, struct aec_info* info);
+
+/* Get reference audio samples + timestamp, in the format expected by AEC,
+ * i.e. same sample rate and bit rate as microphone audio.
+ * Timestamp is updated in field 'timestamp_usec', and not in 'timestamp'.
+ * Returns:
+ *  -EINVAL    if the AEC object is invalid.
+ *  -ENOMEM    if the reference FIFO overflows or is corrupted.
+ *  -ETIMEDOUT if we timed out waiting for the requested number of bytes
+ *  0          otherwise */
+int get_reference_samples(struct aec_t* aec, void* buffer, struct aec_info* info);
+
+#ifdef AEC_HAL
+
+/* Processing function call for AEC.
+ * AEC output is updated at location pointed to by 'buffer'.
+ * This function does not run AEC when there is no playback -
+ * as communicated to this AEC interface using aec_set_spk_running().
+ * Returns -EINVAL if processing fails, else returns 0. */
+int process_aec(struct aec_t* aec, void* buffer, struct aec_info* info);
+
 #else /* #ifdef AEC_HAL */
 
-#define write_to_reference_fifo(...) ((int)0)
 #define process_aec(...) ((int)0)
-#define init_aec(...) ((int)0)
-#define release_aec(...) ((void)0)
-#define init_aec_reference_config(...) ((int)0)
-#define init_aec_mic_config(...) ((int)0)
-#define destroy_aec_reference_config(...) ((void)0)
-#define destroy_aec_mic_config(...) ((void)0)
-#define aec_set_spk_running(...) ((void)0)
 
 #endif /* #ifdef AEC_HAL */
 