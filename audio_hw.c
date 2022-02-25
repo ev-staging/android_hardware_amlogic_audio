snappautomotive/device_amlogic_yukawa

yukawa: audio: Fix compilation warning
device/amlogic/yukawa/hal/audio/audio_hw.c:119:47: warning: format
specifies type 'int *' but the argument has type 'int16_t *' (aka 'short
*') [-Wformat]
        int n = sscanf(line, "%" PRIi16 "\n", &filter[num_taps++]);
                              ~~~~~~~~~       ^~~~~~~~~~~~~~~~~~~

Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
Change-Id: I02af8b4ce555a772bef7a4a0b5117d70fef7c31b

hal/audio/audio_hw.c
@@ -116,12 +116,12 @@ static int read_filter_from_file(const char* filename, int16_t* filter, int max_
         if ((line[0] == '#') || (size < 2)) {
             continue;
         }
-        int n = sscanf(line, "%" PRIi16 "\n", &filter[num_taps++]);
+        int n = sscanf(line, "%" SCNd16 "\n", &filter[num_taps++]);
         if (n < 1) {
             ALOGE("Could not find coefficient %d! Exiting...", num_taps - 1);
             return 0;
         }
-        ALOGV("Coeff %d : %" PRIi16, num_taps, filter[num_taps - 1]);
+        ALOGV("Coeff %d : %" SCNd16, num_taps, filter[num_taps - 1]);
         if (num_taps == max_length) {
             ALOGI("%s: max tap length %d reached.", __func__, max_length);
             break;

ev-staging/android_hardware_amlogic_audio

audio_hw.c
@@ -116,12 +116,12 @@ static int read_filter_from_file(const char* filename, int16_t* filter, int max_
         if ((line[0] == '#') || (size < 2)) {
             continue;
         }
-        int n = sscanf(line, "%" PRIi16 "\n", &filter[num_taps++]);
+        int n = sscanf(line, "%" SCNd16 "\n", &filter[num_taps++]);
         if (n < 1) {
             ALOGE("Could not find coefficient %d! Exiting...", num_taps - 1);
             return 0;
         }
-        ALOGV("Coeff %d : %" PRIi16, num_taps, filter[num_taps - 1]);
+        ALOGV("Coeff %d : %" PRId16, num_taps, filter[num_taps - 1]);
         if (num_taps == max_length) {
             ALOGI("%s: max tap length %d reached.", __func__, max_length);
             break;